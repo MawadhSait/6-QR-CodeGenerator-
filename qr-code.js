bf981be20bdb2b332e4f5d503fe58b55d5e939f9
MawadhSait <48652540+MawadhSait@users.noreply.github.com>
2022-10-27 11:18:41 +0300

    final commit
    calling api to read text and convert it to QRCode

---
+++ qr-code.js
@@ -0,0 +1,23 @@
+const wrapper =document.querySelector(".wrapper")
+const generateBtn =wrapper.querySelector(".form button");
+const qrInput = wrapper.querySelector(".form input")
+const qrImg =wrapper.querySelector(".qr-code img")
+
+generateBtn.addEventListener("click", () =>{
+    let qrValue = qrInput.value
+    if(!qrValue)return //if the feiled is empty return
+    generateBtn.innerText = "Generating QR Code ..."
+    //call the api 
+    qrImg.src=`https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    qrImg.addEventListener("load", () =>{
+        wrapper.classList.add("active")
+        generateBtn.innerText = " Generator QR Code "
+    })
+
+})
+
+qrInput.addEventListener("keyup", () =>{
+    if(qrInput.value){
+        wrapper.classList.remove("active")
+    }
+})
